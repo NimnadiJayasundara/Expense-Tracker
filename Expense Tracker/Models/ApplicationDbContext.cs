b5db206608d10c5693a862c49d07cd7749e194d1
NimnadiJayasundara <nimnadijmh.21@uom.lk>
2025-03-15 13:58:51 +0530

    Add project files.

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext   // inherit from dbcontext cls 
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {}
+
+        public DbSet<Transactions> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
---
+++ Expense Tracker/Models/Transactions.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transactions
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } // forign key
+
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
